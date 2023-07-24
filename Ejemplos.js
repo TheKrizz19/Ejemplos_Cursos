69f03dfc932cb4658136da1686a2f6be8fe260e4
TheKrizz <cristian56navarro@hotmail.com>
2023-07-24 17:01:17 -0600

    Funamentos-de-Javascript-24/07/2023
    Este primer commit abarca todos los temas del primer Módulo del curso fundamentos de javascript

---
+++ Ejemplos.js
@@ -0,0 +1,101 @@
+/*
+var texto= "soy un texto"
+console.log(texto)
+
+var edad= 21
+console.log(edad)
+
+var promedio = 9.5
+console.log(promedio)
+
+var x= -10
+console.log(x)
+
+var futbolista= true
+console.log(futbolista)
+
+var basquetbolista= false
+console.log(basquetbolista)
+
+var nose;
+console.log(nose)
+
+var nada= null
+console.log(nada)
+
+Este fragmento de código hace referencia al tema de variables y tipos de datos del curso 
+Fundamentos de javascript como a su vez también hacemos el uso del siguiente tema que es el uso de comentarios.
+*/
+
+/*var a= 5, b =10, c=7;
+var suma=a+b
+console.log("La suma es: ", suma)
+
+var resta=a-b
+console.log("La resta es: ", resta)
+
+var multiplicar=a*b
+console.log("La multiplicación es: ", multiplicar)
+
+var dividir=a/b
+console.log("La división es: ", dividir)
+
+var modulo=a%b
+console.log("El módulo es: ", modulo)
+
+var suma1=(a+b)*c
+console.log("La otra suma es: ", suma1)
+
+var resta1=(a-b)/c
+console.log("La nueva resta es: ", resta1)
+
+var operacion=(a+b) * (a-c)
+
+Este fragmento de código hace referencia al tema de operadores lógicos del curso Fundamentos de javascript
+*/
+
+
+/*
+var contador= 2
+contador ++
+console.log("El resultado es: ", contador)
+contador --
+console.log("El resultado es: ", contador)
+
+var numero=1, numero1=10
+numero +=2
+numero1 -=5
+console.log("El resultado es: ", numero)
+console.log("El resultado es: ", numero1)
+
+Este fragmento de código hace referencia al tema incremento y decremento del curso Fundamentos de javascript
+*/
+
+/*var saludo="Hola ", nombre="Soy Cristian", apellido="Navarro", juegofav=" Mijuego favorito es Resident Evil", edad=21
+console.log(saludo + nombre + " mi apellido es " +  apellido +  juegofav + " mi edad es de "+ edad)
+
+Aquí hago uso de la concatención de variables que es un tema abarcado en el curdo de Fundamentos de javascript
+*/
+
+/*var num= 5
+console.log(typeof(num))
+var nombre="Leon S. Kennedy"
+console.log(typeof(nombre))
+var num1 =-10
+console.log(typeof(num1))
+var num2=1.45
+console.log(typeof(num2))
+var r=5*"juan"
+console.log(typeof(r))
+var s="111" + 1
+console.log(typeof(s))
+var s1="10" - 1
+console.log(typeof(s1))
+var numeros= [1,2,3,4,5]
+console.log(typeof(numeros))
+var CR7;
+console.log(typeof(CR7))
+var YN=true
+console.log(typeof(YN))
+Aquí hago referencia a como verificar los tipos de variables usando la función typeof de javascript
+*/
