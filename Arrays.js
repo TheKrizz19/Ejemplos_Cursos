e5d1706f03f90410bdf54af60d673d2cf6e9da00
TheKrizz <cristian56navarro@hotmail.com>
2023-07-24 22:04:36 -0600

    Fundamentos-de-Javascript-24/07/2023
    Ejemplo de uso de un arreglo y su recorrido

---
+++ Arrays.js
@@ -0,0 +1,14 @@
+var numeros = [10, 5, 25, 3, 8, 15];
+var numMayor=0
+var numMenor=0
+for(i=0;i< numeros.length;i++){
+    if(numeros[i]>numMayor)
+    numMayor=numeros[i]
+}
+if(numeros[i]<numMenor){
+    numMenor=numeros[i]
+
+}
+console.log("El numero mayor es: " + numMayor)
+console.log("El numero menor es: "+ numMenor)
+//Este código ejemplifica el uso de un arreglo y su recorrido
